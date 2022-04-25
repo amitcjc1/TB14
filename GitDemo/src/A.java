ecc39b149c671cec57ecd4cc56783875ddcec6b5
amit <amitcjcpune@gmail.com>
2022-04-25 10:50:50 +0530

    By Amit25

--- GitDemo/src/A.java
+++ GitDemo/src/A.java
@@ -2,7 +2,16 @@
 public class A 
 {
 
-	public static void main(String[] args) {
+	public void m1()
+	{
+		System.out.println("This is M1 method");
+	}
+	
+	
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello A");
+		A a=new A();
+		a.m1();
 	}
 }
