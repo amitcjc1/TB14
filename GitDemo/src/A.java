6676f3cd9e4352fce55b0e1ef666288970346eab
amitcjc1 <61121166+amitcjc1@users.noreply.github.com>
2022-04-25 11:03:32 +0530

    Branch1

--- GitDemo/src/A.java
+++ GitDemo/src/A.java
@@ -11,6 +11,11 @@ public class A
 		System.out.println("This is M2 method");
 	}
 	
+	public void m3()
+	{
+		System.out.println("This is M3 method");
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello A");
